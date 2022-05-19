41c7192d8a3e13f97194694cb32888abdd65c53f
Hurkan Kalan <hurkan.kalan@gmail.com>
2022-05-19 10:01:41 +0200

    change config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from "next";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Authentification from "./Authentification";
 
 const Home: NextPage = () => {
