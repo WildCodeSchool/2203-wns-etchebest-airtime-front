692b25b89a908b632dc50552c73af419e76f0e74
YoannBoisson <yoann.boisson@live.fr>
2022-05-20 11:29:20 +0200

    CRUD front side for ticket

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import '../styles/globals.css';
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
@@ -12,6 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
+      <ToastContainer />
     </ApolloProvider>
   );
 }
