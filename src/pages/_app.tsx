3071cc6208105be8ac2a3efc1faa63e4e24e45af
YoannBoisson <yoann.boisson@live.fr>
2022-06-16 10:34:30 +0200

    test src folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import '../styles/globals.css';
 import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
